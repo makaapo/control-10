28b5939828963a99e87d3c630f6357398f124f4b
Karimov Abdumalik <abdumal1k.ur@gmail.com>
2024-08-24 13:26:05 +0600

    Создал hooks и store для redux, добавил тулбар, файл темы и отобразил все в app и main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,17 @@
+import {Container} from '@mui/material';
+import {Route, Routes} from 'react-router-dom';
+import AppToolbar from './UI/AppToolbar/AppToolbar';
 const App = () => (
-    <>
-        da
-    </>
+  <>
+    <header>
+      <AppToolbar/>
+    </header>
+    <Container maxWidth="xl" component="main" sx={{marginTop: '2rem'}}>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}/>
+      </Routes>
+    </Container>
+  </>
 );
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+import {Provider} from 'react-redux';
+import {store} from './app/store';
+import {BrowserRouter} from 'react-router-dom';
+import {CssBaseline, ThemeProvider} from '@mui/material';
+import theme from './theme';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
+  </StrictMode>
 )
